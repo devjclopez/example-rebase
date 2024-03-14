90457855dd90aa91ab9da5d9ba035095c2af7656
Josimar Albert Castro Lopez <dev.jclopez@gmail.com>
2024-03-14 12:46:46 -0500

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header"
+
+export const Home = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Home Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt minus, pariatur eum quam dolor corporis? Aliquid cumque, fugit soluta possimus commodi ipsum, ex alias facilis et consectetur minima, ducimus tenetur.</p>
+    </div>
+  )
+}
